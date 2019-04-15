9ae684c49b94da29919a256a85d2d931310eb339
lqh <2663237779@qq.com>
2019-04-15 17:31:47 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/preferece/PreFragment.java
@@ -0,0 +1,12 @@
+package com.example.administrator.preferece;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+public class PreFragment extends PreferenceFragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //从xml文件中加载选项
+        addPreferencesFromResource(R.xml.preference);
+    }
+}
